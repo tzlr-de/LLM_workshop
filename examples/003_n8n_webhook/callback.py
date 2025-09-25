cf92c5c9830961c6c69c5b15ae80bc8743615dcb
Dimitri Korsch <korschdima@gmail.com>
2025-09-25 16:03:54 +0200

    added folder watcher, which can communicate with n8n and get response from it

---
+++ examples/003_n8n_webhook/callback.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import json
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+from dataclasses import dataclass
+
+@dataclass
+class Data:
+    receipt_id: str
+    issuer: str
+    date: str
+    net_price: float
+    vat_pcent: float
+    vat: float
+    price_total: float
+
+
+def process(data: str):
+    print("Received data:")
+    content = json.loads(data)
+    data = Data(**content)
+    print(data)
+
+
+class Handler(BaseHTTPRequestHandler):
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
+        process(post_data.decode('utf-8'))
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b'{"status": "received"}')
+
+def main(args):
+    server_address = (args.host, args.port)
+    httpd = HTTPServer(server_address, Handler)
+    print(f"Starting server on {args.host}:{args.port}")
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(description="n8n Webhook Callback Listener")
+
+    parser.add_argument("--host", type=str, default="localhost")
+    parser.add_argument("--port", type=int, default=8088)
+    main(parser.parse_args())
---
+++ examples/003_n8n_webhook/watcher.py
@@ -0,0 +1,84 @@
+#! /usr/bin/env python3
+
+import requests
+import time
+
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+
+class n8nHandler(FileSystemEventHandler):
+    def __init__(self, url, method="POST"):
+        super().__init__()
+        self._url = url
+        self._method = method
+
+    def on_created(self, event):
+        ftype = "Directory" if event.is_directory else "File"
+        print(f"{ftype} created: {event.src_path}")
+        if not event.is_directory:
+            self._send_request(event.src_path)
+
+    def on_modified(self, event):
+        ftype = "Directory" if event.is_directory else "File"
+        print(f"{ftype} modified: {event.src_path}")
+
+    def _send_request(self, path):
+        response = requests.request(
+            method=self._method,
+            url=self._url,
+            files={"new_file": open(path, "rb")},
+            headers={"Content-Type": "application/binary"},
+        )
+
+        content = response.json()
+        code = content.get("code", "unknown")
+        msg = content.get("message", "")
+        hint = content.get("hint", "")
+
+        if response.status_code != 200:
+            print(f"ERROR ({code=})")
+            print(msg)
+            print(f"HINT: {hint}")
+        else:
+            print("SUCCESS")
+            print(msg)
+
+def main(args):
+
+    base_url = f"{args.host}:{args.port}{args.hook_path}"
+    if args.is_test:
+        base_url += "-test"
+
+    url = f"{base_url}/{args.hook_uuid}"
+    observer = Observer()
+
+    handler = n8nHandler(url=url, method=args.method)
+    observer.schedule(handler, path=args.path, recursive=False)
+
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
+
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(description="n8n Webhook Example")
+
+    parser.add_argument("--host", type=str, default="http://192.168.1.45")
+    parser.add_argument("--port", type=int, default=5678)
+    parser.add_argument("--hook_uuid", "-uuid", type=str,
+                        default="caa9b92e-e9ee-466a-a2cc-85d1fb6a2a8a",
+                        help="The UUID of the n8n webhook")
+
+    parser.add_argument("--hook_path", type=str, default="/webhook")
+    parser.add_argument("--method", "-m", type=str, default="POST", choices=["GET", "POST", "PUT", "DELETE"])
+    parser.add_argument("--is_test", "-test", action="store_true", help="Run in test mode")
+
+    parser.add_argument("--path", "-p", type=str, default="folder", help="Path to the document directory")
+
+    main(parser.parse_args())
