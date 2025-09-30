1076ac323b69cf1277b24734f9729f9ee35d69cd
Dimitri Korsch <korschdima@gmail.com>
2025-09-30 15:00:03 +0200

    updated examples

--- examples/003_n8n_webhook/callback.py
+++ examples/003_n8n_webhook/callback.py
@@ -43,6 +43,6 @@ if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser(description="n8n Webhook Callback Listener")
 
-    parser.add_argument("--host", type=str, default="localhost")
+    parser.add_argument("--host", type=str, default="0.0.0.0")
     parser.add_argument("--port", type=int, default=8088)
     main(parser.parse_args())
